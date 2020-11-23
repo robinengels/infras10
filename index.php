82e83884618c2409ed23a693ff47813126f0f063
Jacana <32939472+robinengels@users.noreply.github.com>
2020-11-23 11:41:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "Hello world";
 echo "Il est l'heure de manger";
 echo "Ce soir, c'est soupe aux champignons";
 echo "Coucou";
-echo "Salut à vous";
+echo "Salut nigger";
    echo "Le site des bonnes nouvelles à changé ma vie, depuis le soleil est rayonnant";
 
    echo "<h1> VIVE LES CACAHUÈTES </h1>";
